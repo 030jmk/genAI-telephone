169f95d6c0e93ae0e4c0471a831e4900148d2258
Jan Kopankiewicz <030jmk@users.noreply.github.com>
2023-08-15 14:32:14 +0200

    Update whisper.py
    speech key

--- whisper.py
+++ whisper.py
@@ -117,10 +117,10 @@ def generate_text_to_speech(generated_text, lang='en-US', gender='Female', name=
     else:
         print("Error:", response.status_code)
 
-def generate_text_to_answer(generated_text, lang='en-US', gender='Female', name='en-US-JennyNeural', style=None, service_region="germanywestcentral"):
+def generate_text_to_answer(generated_text, lang='en-US', gender='Female', name='en-US-JennyNeural', style=None, service_region="germanywestcentral", speech_key=speech_key):
     url = "https://{}.tts.speech.microsoft.com/cognitiveservices/v1".format(service_region)
     headers = {
-        "Ocp-Apim-Subscription-Key": "01ff10d899f5494abc7b0a114256b849",
+        "Ocp-Apim-Subscription-Key": speech_key,
         "Content-Type": "application/ssml+xml",
         "X-Microsoft-OutputFormat": "audio-16khz-64kbitrate-mono-mp3",
         "User-Agent": "curl"
